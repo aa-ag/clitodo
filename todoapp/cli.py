6f7d591fba68a708d8730f31cadbd8e0bdae84f1
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-10 20:19:46 -0600

    added main function

--- todoapp/cli.py
+++ todoapp/cli.py
@@ -11,3 +11,16 @@ def _version_callback(value: bool) -> None:
     if value:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
