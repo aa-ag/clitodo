421d6175243aef5f04887009b9c94b25f43a7541
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-10 20:19:26 -0600

    added app object & _version_callback function

--- todoapp/cli.py
+++ todoapp/cli.py
@@ -4,3 +4,10 @@
 from typing import Optional
 import typer
 from todo import __app_name__, __version__
+
+app = typer.Typer()
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
