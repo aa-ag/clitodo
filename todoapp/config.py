05f88dc83bfd2eea45f6f3c4b35b246689af84f8
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-22 21:47:19 -0600

    added _init_config_file function

--- todoapp/config.py
+++ todoapp/config.py
@@ -13,6 +13,7 @@ from todoapp import (
 CONFIG_DIR_PATH = Path(typer.get_app_dir())
 CONFIG_FILE_PATH = CONFIG_DIR_PATH / "config.ini"
 
+
 def init_app(db_path: str) -> int:
     """Initialize the application."""
     config_code = _init_config_file()
@@ -21,4 +22,16 @@ def init_app(db_path: str) -> int:
     database_code = _create_database(db_path)
     if database_code != SUCCESS:
         return database_code
+    return SUCCESS
+
+
+def _init_config_file() -> int:
+    try:
+        CONFIG_DIR_PATH.mkdir(exist_ok=True)
+    except OSError:
+        return DIR_ERROR
+    try:
+        CONFIG_FILE_PATH.touch(exist_ok=True)
+    except OSError:
+        return FILE_ERROR
     return SUCCESS
