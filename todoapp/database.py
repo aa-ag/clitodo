a15d3aeaa4f716bfa2d1ea775681fac342eab6d7
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-05 19:02:07 -0600

    added get_database_path function

--- todoapp/database.py
+++ todoapp/database.py
@@ -8,3 +8,9 @@ DEFAULT_DB_FILE_PATH = Path.home().joinpath(
     "." + Path.home().stem + "_todo.json"
 )
 
+def get_database_path(config_file: Path) -> Path:
+    """Return the current path to the to-do database."""
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file)
+    return Path(config_parser["General"]["database"])
+
